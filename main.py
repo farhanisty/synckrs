43459e4fa230a2fa2517354b55cc2caf327c3968
farhanisty <farhannivtasija@gmail.com>
2024-07-29 11:38:04 +0700

    create si matkul scraper

--- main.py
+++ main.py
@@ -1,44 +1,45 @@
 from src.FileOperation import FileOperation
 from src.MatkulScraper import InformatikaMatkulScraper
+from src.MatkulScraper import SistemInformasiMatkulScraper
 from src.ScheduleCreator import ScheduleCreator
 from src.sorter.DosenSorter import DosenSorter
 from src.sorter.MatkulNameSorter import MatkulNameSorter
 from src.sorter.IdMatkulSorter import IdMatkulSorter
-from src.cluster.DosenMatkulCluster import DosenMatkulCluster
 
-listMatkulSem = [
-    "Bahasa Indonesia",
-    "Struktur Data",
-    "Komputer dan Masyarakat",
-    "Komputasi Numerik",
-    "Interaksi Manusia dan Komputer",
-    "Sistem Digital",
-    "Geoinformatika",
-    "Pemrograman Web",
-    "Praktikum Implementasi Struktur Data",
-    "Praktikum Pemrograman Web",
-    "Bela Negara dan Widya Mwat Yasa",
-    "Riset Operasi",
-]
 
-scrapper = InformatikaMatkulScraper()
-hasil = scrapper.union(listMatkulSem, scrapper.generate(
-    FileOperation.read("data.src"))
+scrapper = SistemInformasiMatkulScraper()
+hasil = scrapper.union(scrapper.getMatkulList(), scrapper.generate(
+    FileOperation.read("matkulsi.src"))
 )
-
 
 scheduleCreator = ScheduleCreator(hasil)
 
-scheduleCreator.choose(23)
-scheduleCreator.choose(34)
-scheduleCreator.choose(111)
+scheduleCreator.showAvailable()
 
-scheduleCreator.getRenderInvoker().sort(IdMatkulSorter())
-scheduleCreator.showChoosen()
-
-scheduleCreator.showUnavailable()
-# scheduleCreator.getRenderInvoker().sort(DosenSorter()).changeMode("DESC")
-# scheduleCreator.showAvailable()
+# scheduleCreator.choose(70)
+# scheduleCreator.choose(47)
+# scheduleCreator.choose(34)
+# scheduleCreator.choose(84)
+# scheduleCreator.choose(104)
+# scheduleCreator.choose(156)
+# scheduleCreator.choose(126)
+# scheduleCreator.choose(141)
+# scheduleCreator.choose(111)
+# scheduleCreator.choose(124)
+# scheduleCreator.choose(173)
+# scheduleCreator.choose(147)
+# # scheduleCreator.choose(23)
+# # scheduleCreator.choose(48)
 #
-# scheduleCreator.getRenderInvoker().sort(IdMatkulSorter()).changeMode("ASC")
-# scheduleCreator.showAvailable()
+# scheduleCreator.getRenderInvoker().sort(IdMatkulSorter())
+# scheduleCreator.showChoosen()
+#
+# # scheduleCreator.showUnavailable()
+# # scheduleCreator.getRenderInvoker().sort(MatkulNameSorter())
+# # scheduleCreator.showAvailable()
+#
+# scheduleCreator.getRenderInvoker().sort(MatkulNameSorter())
+# scheduleCreator.showUnavailable()
+# #
+# # scheduleCreator.getRenderInvoker().sort(IdMatkulSorter()).changeMode("ASC")
+# # scheduleCreator.showAvailable()
--- src/MatkulScraper.py
+++ src/MatkulScraper.py
@@ -9,6 +9,10 @@ class MatkulScraper(ABC):
     def generate(self, list_string: list[str]) -> list[Matkul]:
         pass
 
+    @abstractmethod
+    def getMatkulList(self):
+        pass
+
     def union(self, list_in: list[str], list_matkuls: list[Matkul]) -> list[Matkul]:
         return list(filter(lambda x: x.nama.strip()
                            in list_in, list_matkuls))
@@ -40,3 +44,60 @@ class InformatikaMatkulScraper(MatkulScraper):
             result.append(res)
 
         return result
+
+    def getMatkulList(self):
+        return [
+            "Bahasa Indonesia",
+            "Struktur Data",
+            "Komputer dan Masyarakat",
+            "Komputasi Numerik",
+            "Interaksi Manusia dan Komputer",
+            "Sistem Digital",
+            "Geoinformatika",
+            "Pemrograman Web",
+            "Praktikum Implementasi Struktur Data",
+            "Praktikum Pemrograman Web",
+            "Bela Negara dan Widya Mwat Yasa",
+            "Riset Operasi",
+        ]
+
+
+class SistemInformasiMatkulScraper(MatkulScraper):
+    def generate(self, list_string: list[str]) -> list[Matkul]:
+        result = list[Matkul]()
+
+        idx = 1
+        for x in list_string:
+            if x == "\n":
+                continue
+
+            if not re.search("^SISTEM INFORMASI", x):
+                result[-1].dosen.append(x.strip())
+                continue
+
+            matkul = re.split("\t", x)
+
+            res = Matkul(idx, matkul[0], matkul[1], matkul[2], matkul[3],
+                         matkul[4], matkul[5],
+                         Jadwal.buildFromString(matkul[6]), matkul[7])
+
+            idx += 1
+            result.append(res)
+
+        return result
+
+    def getMatkulList(self):
+        return [
+            "Perencanaan Strategis TI",
+            "Rekayasa Kebutuhan Perangkat Lunak",
+            "Sistem Pendukung Keputusan",
+            "Keamanan Aset Informasi",
+            "Desain & Manajemen Jaringan Komputer",
+            "Metode Survey dan Pengolahan Data",
+            "Interaksi Manusia & Komputer",
+            "Kapita Selekta",
+            "Sistem Informasi Manajemen Bencana",
+            "Praktikum Desain & Manajemen Jaringan Komputer",
+            "Praktikum Rekayasa Kebutuhan Perangkat Lunak",
+            "Etika Profesi TI",
+        ]
